3cda5000959b5a5ec521eb39e50db0ded88f317b
WIL_FU_ <wilfudev@gmail.com>
2025-10-15 21:13:17 +1030

    Initial commit

---
+++ ddosr.py
@@ -0,0 +1,19 @@
+#ddos-r version 1.0.0
+
+#cool ascii art :3
+print(r"▓█████▄ ▓█████▄  ▒█████    ██████          ██▀███  "+"\n"
+      r"▒██▀ ██▌▒██▀ ██▌▒██▒  ██▒▒██    ▒         ▓██ ▒ ██▒"+"\n"
+      r"░██   █▌░██   █▌▒██░  ██▒░ ▓██▄   ░▒▓█▓▒░ ▓██ ░▄█ ▒"+"\n"
+      r"░▓█▄   ▌░▓█▄   ▌▒██   ██░  ▒   ██▒        ▒██▀▀█▄  "+"\n"
+      r"░▒████▓ ░▒████▓ ░ ████▓▒░▒██████▒▒        ░██▓ ▒██▒"+"\n"
+      r" ▒▒▓  ▒  ▒▒▓  ▒ ░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░        ░ ▒▓ ░▒▓░"+"\n"
+      r" ░ ▒  ▒  ░ ▒  ▒   ░ ▒ ▒░ ░ ░▒  ░ ░          ░▒ ░ ▒░"+"\n"
+      r" ░ ░  ░  ░ ░  ░ ░ ░ ░ ▒  ░  ░  ░            ░░   ░ "+"\n"
+      r"   ░       ░        ░ ░        ░    version 1.0.0"+"\n"
+      r"a dead simple distributed denial-of-service tool"+"\n"
+      r"by WIL_FU_"+"\n"
+      r"in case you skids didn't get the memo, this is for ETHICAL PURPOSES ONLY"+"\n"
+      "ONLY USE ON SYSTEMS YOU HAVE BEEN GIVEN EXPLICIT PERMISSION TO TEST ON")
+def attack_type(num):
+    print("----ATTACK MENU----")
+    print("|                 |")
